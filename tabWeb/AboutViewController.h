e9d6c638af2a97788476dc3ae88f36a7bfb59702
rnjai <rnjailamba12@gmail.com>
2016-08-02 07:12:15 +0530

    web view works now

---
+++ tabWeb/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  tabWeb
+//
+//  Created by Mr Ruby on 02/08/16.
+//  Copyright © 2016 Rnjai Lamba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+@end
